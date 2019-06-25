7b344e62f67ab7977347a78256c9ea2a7348353f
Jaydeep <jaydeep7karena7@gmail.com>
2019-06-25 22:56:11 +0530

    added hader-menu component

---
+++ src/components/HeaderMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HeaderMenu() {
+  return (
+    <>
+      <p className="header-menu-container">
+        <img src="" alt="logo here"/>
+        <a href="">Create Post</a>
+        <div>Username</div>
+      </p>
+    </>
+  );
+}
+
+export default HeaderMenu;
