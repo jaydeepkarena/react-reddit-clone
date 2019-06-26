95f549b2486a9a97a2cfee5b5a95c995e4c4e720
Jaydeep Karena <jaydeep@weenggs.com>
2019-06-26 14:30:11 +0530

    desinged top menu bar

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import logo from '../assets/images/reddit_logo.svg';
+import './HeaderMenu.css';
 
 function HeaderMenu() {
   return (
     <>
       <p className="header-menu-container">
-        <img src="" alt="logo here"/>
-        <a href="">Create Post</a>
-        <div>Username</div>
+        <div className="logo">
+          <img src={logo} alt="logo here" className="reddit-logo" />
+          <span>Reddit</span>
+        </div>
+        <a href="" className="crate-post" >Create Post</a>
+        <div className="user-name">Username</div>
       </p>
     </>
   );
