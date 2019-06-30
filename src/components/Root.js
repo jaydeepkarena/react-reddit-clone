8728e68aae11ade7a600155c042a0a075c3aa8cd
Jaydeep <jaydeep7karena7@gmail.com>
2019-06-30 16:00:46 +0530

    x

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import App from '../App';
+import Login from './Login';
+import SignUp from './SignUp';
 
 const Root = ({ store }) => {
   return (
     <Router>
       <Provider store={store}>
-        <Route path="/" component={App} />>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
       </Provider>
     </Router>
   );
