1cd5f496300b2c8157cfa5dfec35c821c8289d17
Jaydeep <jaydeep7karena7@gmail.com>
2019-07-02 00:12:05 +0530

    x

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-import { createStore } from 'redux';
 import rootReducer from './store/reducers';
-import Root from './components/Root'
+import Root from './components/Root';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
