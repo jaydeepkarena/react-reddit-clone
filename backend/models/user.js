938c0fad26e81e230990ba4844e196aa503776c6
Jaydeep <jaydeep7karena7@gmail.com>
2019-07-11 22:59:09 +0530

    created login and signup routes

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    minlength: 3,
     trim: true,
     lowercase: true,
     unique: true
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,47 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { User } = require('./models/user');
+const { User, validateUser } = require('./models/user');
 
-mongoose.connect('mongod://localhost:27017/react-reddit-clone', { useNewUrlParser: true });
+mongoose
+  .connect('mongodb://localhost/react-reddit-clone', { useNewUrlParser: true })
+  .then(() => console.log('Successfully connected to MongoDb...'))
+  .catch(err => console.log(err));
 
 app.use(express.json());
 
-process.on('', (err) => console.log(err))
+process.on('unhandledRejection', err => console.log(err));
 
-app.post('/api/login', async (req, res) => {
-  const user = await User.findOne({ email: req.params.email, password: req.params.password });
+app.post('/auth/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
   if (user) return res.send('Login successful!');
 
-  res.status(400).send('Invalid email or password!');
+  res.status(401).send('Invalid email or password!');
 });
 
-app.post('/api/signup', (req, res) => {
-  return res.send('Coming soon....');
-  // const { error } = validateUser(req.body);
-  // console.log(error.details[0].message);
-  // if (error) {
-  //   return res.send(error.details[0].message);
-  // }
-  // const newId = uuid();
-  // const newUser = { [newId]: { ...req.body, id: newId } };
-  // users = { ...users, ...newUser };
-  // res.send(newuser);
+app.post('/auth/signup', async (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const { email, password } = req.body;
+  let user = await User.findOne({ email, password });
+  if (user) return res.status(401).send('User already registered!');
+
+  try {
+    user = new User({ email, password });
+    await user.save();
+    return res.send(user);
+  } catch (e) {
+    console.log('ERRORORORR:', e);
+  }
+  return res.send(user);
 });
 
-const PORT = process.env.PORT || 8080;
+app.get('/auth/users', async (req, res) => {
+  const users = await User.find({});
+  console.log(users);
+  res.send(users);
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
