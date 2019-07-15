284758a7e4715ec146518778a7bb4fe8d8192af3
Jaydeep <jaydeep7karena7@gmail.com>
2019-07-15 23:55:56 +0530

    while creating post if user logs out then redirect to home

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-const NewPost = (props)=>{
-return <h1>New Post</h1>
-}
+const NewPost = props => {
+  if (!props.currentUserId) {
+    return <Redirect to="/" />;
+  }
+  return (
+    <>
+      <div className="new-post">
+        <h1>New Post</h1>
+        <input type="text" name="post-title" placeholder="Title" />
+        <textarea name="post-description" cols="30" rows="10" placeholder="Descripton(optional)" />
+        <div className="buttons">
+          <button>Submit</button>
+          <button>Discard</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default NewPost;
+const mapStateToProps = ({ data: { currentUserId } }) => ({
+  currentUserId
+});
+
+export default connect(mapStateToProps)(NewPost);
