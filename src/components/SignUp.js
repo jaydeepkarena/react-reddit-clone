89af2594d070ca5313a7293410b2394cc5c15eca
Jaydeep Karena <jaydeep@weenggs.com>
2019-07-15 15:06:44 +0530

    added some styling to signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import Joi from '@hapi/joi';
 import './SignUp.css';
+import loader from './../assets/images/logo.svg';
 
 const SignUp = () => {
   const [error, setError] = useState('');
@@ -46,26 +47,27 @@ const SignUp = () => {
 
   return (
     <>
-      {showErrors()}
-      <h1 className="signup"> SignUp Component </h1>
-      <div className="signup-items">
-        <div className="control-group">
-          <label htmlFor="name"> Name: </label>
-          <input type="text" name="name" id="name" ref={nameRef} />
-        </div>
-        <div className="control-group">
-          <label htmlFor="email"> Email: </label>
-          <input type="text" name="email" id="email" ref={emailRef} />
-        </div>
-        <div className="control-group">
-          <label htmlFor="password"> Password: </label>
-          <input type="password" name="password" id="password" ref={passwordRef} />
-        </div>
-        <div className="control-group">
-          <label htmlFor="password2"> Confirm Password: </label>
-          <input type="password" name="password2" id="password2" ref={confirmPasswordRef} />
-        </div>
-        <div className="control-group">
+      <div className="signup">
+        <div className="signup-items">
+          <img src={loader} alt="wait..." className="loader" height="100" width="100" />
+          <h1> SignUp </h1>
+          {showErrors()}
+          <div className="control-group">
+            <label htmlFor="name"> Name: </label>
+            <input type="text" name="name" id="name" ref={nameRef} />
+          </div>
+          <div className="control-group">
+            <label htmlFor="email"> Email: </label>
+            <input type="text" name="email" id="email" ref={emailRef} />
+          </div>
+          <div className="control-group">
+            <label htmlFor="password"> Password: </label>
+            <input type="password" name="password" id="password" ref={passwordRef} />
+          </div>
+          <div className="control-group">
+            <label htmlFor="password2"> Confirm Password: </label>
+            <input type="password" name="password2" id="password2" ref={confirmPasswordRef} />
+          </div>
           <button onClick={submit}> Submit </button>
           <button onClick={reset}> Reset </button>
         </div>
@@ -80,7 +82,9 @@ const validate = user => {
       .required()
       .min(5)
       .max(255),
-    email: Joi.email().required(),
+    email: Joi.string()
+      .email()
+      .required(),
     password: Joi.string()
       .required()
       .min(5)
