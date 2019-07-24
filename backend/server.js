779176413291e0cd8a7a767bf52ed99632c90fa5
Jaydeep <jaydeep7karena7@gmail.com>
2019-07-24 21:51:25 +0530

    endpoint for sorting and top N post

--- backend/server.js
+++ backend/server.js
@@ -119,7 +119,9 @@ app.delete('/remove-all-post', async (req, res) => {
 });
 
 app.get('/posts', async (req, res) => {
-  const posts = await Post.find().sort({ timestamp: -1 }).limit(5);
+  const posts = await Post.find()
+    .sort({ timestamp: -1 })
+    .limit(5);
   console.log(posts);
   res.send(posts);
 });
