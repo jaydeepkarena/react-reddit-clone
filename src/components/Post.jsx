76ca121df226e3dd5d19f20819cfc432e0ec79af
Jaydeep <jaydeep7karena7@gmail.com>
2019-07-25 23:25:47 +0530

    dynamically shown delete button for post if it is created by current user

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import './Post.css';
 import moment from 'moment';
-import { imageURL } from '../utils/config';
+import { connect } from 'react-redux';
 
-const Post = ({ post }) => {
+import './Post.css';
+import { imageURL } from '../utils/config';
+import API from '../utils/api';
+
+const Post = ({ post, currentUserId }) => {
+  const deletePost = async _id => {
+    const result = await API.delete('post', { id: _id });
+    console.log(`DELETE POST RESULT >>>`);
+    console.log(result);
+  };
+
+  const deleteButton = () => {
+    if (currentUserId === post.user) {
+      return <button onClick={() => deletePost(post._id)}>X</button>;
+    }
+    return null;
+  };
+
   return (
     <>
       <div className="post">
-        <div className="post_title">{post.title}</div>
+        <div className="post_title_group">
+          <div className="post_title">{post.title}</div>
+          {deleteButton()}
+        </div>
         <div className="post_description">{post.description}</div>
         {post.image && <img src={`${imageURL}${post.image}`} alt="post_image" />}
         <div className="post_date">{moment(post.timestamp).fromNow()}</div>
@@ -16,4 +35,9 @@ const Post = ({ post }) => {
   );
 };
 
-export default Post;
+const mapStateToPros = (state, props) => ({
+  currentUserId: state.data.currentUserId,
+  post: props.post
+});
+
+export default connect(mapStateToPros)(Post);
