6d85180ce0064b109ad3298d08d6270e2c2735f1
Jaydeep <jaydeep7karena7@gmail.com>
2019-08-08 23:34:36 +0530

    solved margin issue of header component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Profile.scss';
 
 const Profile = props => {
-  console.log(props)
-  return <div> Profile Component</div>;
+  return <div> Profile Component </div>;
+  console.log(props.match.params.userid);
 };
 
 export default Profile;
